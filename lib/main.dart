46bc6dd2e4bb6ea83a56a9a2087a03f0112c2d1a
sasuk <sasukeamjed@gmail.com>
2021-03-12 19:31:17 +0400

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/all.dart';
+
+
 
 void main(){
   runApp(MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHome extends HookWidget {
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
